2027d2ee47732eb43dc45cae9d710b451f42ef6f
P. Michael Holland <maikeru.horando@gmail.com>
2012-06-10 12:47:51 +0900

    Add have_header rspec matcher and refactor specs to use it

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -23,3 +23,9 @@ Rspec::Matchers.define :have_success_message do |message|
     page.should have_selector('div.alert.alert-success', text: message)
   end
 end
+
+Rspec::Matchers.define :have_header do |message|
+  match do |page|
+    page.should have_selector('h1', text: message)
+  end
+end
